1b96bd20e6974fc069e5510254b2bc3ad06e3526
Glitch (hello-express) <none>
2023-09-04 09:46:49 +0000

    🌺🏘 Checkpoint ./public/app/script.js:663034/284 ./public/app/index.html:663034/3244 ./package.json:663034/1345 ./server.js:663034/40

--- public/app/script.js
+++ public/app/script.js
@@ -1,5 +1,6 @@
 // This will use the demo backend if you open index.html locally via file://, otherwise your server will be used
 let backendUrl = "https://tiktok-chat-reader.zerody.one";
+// let backendUrl = location.protocol === 'file:' ? "https://tiktok-chat-reader.zerody.one/" : undefined;
 let connection = new TikTokIOConnection(backendUrl);
 
 // Counter
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const io = require("socketio");
 
 app.use(express.static("public"));
 
