fd264e5055a769050863764b3e0a0b86f8b708a6
Glitch (hello-express) <none>
2023-09-04 10:55:54 +0000

    👙👰 Checkpoint ./server.js:663034/1841

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express'),
     app = express(),
     http = require('http'),
+    client = require("socket.io-client"),
     socketIO = require('socket.io'),
     server, io;
 
@@ -16,11 +17,69 @@ server.listen(5000);
 
 io = socketIO(server);
 
-io.on('connection', function (socket) {
-  
-});
+class TikTokIOConnection {
+    constructor() {
+        this.socket = client.connect("https://tiktok-chat-reader.zerody.one");
+        this.uniqueId = null;
+        this.options = null;
+
+        this.socket.on('connect', () => {
+            console.info("Socket connected!");
+
+            // Reconnect to streamer if uniqueId already set
+            if (this.uniqueId) {
+                this.setUniqueId();
+            }
+        })
+        
+        this.socket.on('greeting-from-server', (mess) => {
+          console.log(mess);
+        })
+
+        this.socket.on('disconnect', () => {
+            console.warn("Socket disconnected!");
+        })
+
+        this.socket.on('streamEnd', () => {
+            console.warn("LIVE has ended!");
+            this.uniqueId = null;
+        })
+
+        this.socket.on('tiktokDisconnected', (errMsg) => {
+            console.warn(errMsg);
+            if (errMsg && errMsg.includes('LIVE has ended')) {
+                this.uniqueId = null;
+            }
+        });
+    }
+
+    connect(uniqueId, options) {
+        this.uniqueId = uniqueId;
+        this.options = options || {};
+
+        this.setUniqueId();
+
+        return new Promise((resolve, reject) => {
+            this.socket.once('tiktokConnected', resolve);
+            this.socket.once('tiktokDisconnected', reject);
+
+            setTimeout(() => {
+                reject('Connection Timeout');
+            }, 15000)
+        })
+    }
+
+    setUniqueId() {
+        this.socket.emit('setUniqueId', this.uniqueId, this.options);
+    }
+
+    on(eventName, eventHandler) {
+        this.socket.on(eventName, eventHandler);
+    }
+}
 
 io.of('/app').on('connection', function(socket){
-  console.log('new client connected!', socket.id)
+  // console.log('new client connected!', socket.id)
+  
 })
 
