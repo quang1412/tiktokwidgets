6b19dcec04e18d18a6fda0341fc76c4869e4bc5f
Glitch (hello-express) <none>
2023-09-06 18:15:26 +0000

    ☺️🍓 Checkpoint ./public/ranking/script.js:663034/5580

--- public/ranking/script.js
+++ public/ranking/script.js
@@ -32,12 +32,7 @@ function connect() {
             enableExtendedGiftInfo: true
         }).then(state => {
             $('#stateText').text(`Connected to roomId ${state.roomId}`);
-
-            // reset stats
-            viewerCount = 0;
-            likeCount = 0;
-            diamondsCount = 0;
-            updateRoomStats();
+ 
 
         }).catch(errorMessage => {
             $('#stateText').text(errorMessage);
@@ -52,166 +47,9 @@ function connect() {
     } else {
         alert('no username entered');
     }
-}
+} 
 
-// Prevent Cross site scripting (XSS)
-function sanitize(text) {
-    return text.replace(/</g, '&lt;')
-}
-
-function updateRoomStats() {
-    $('#roomStats').html(`Viewers: <b>${viewerCount.toLocaleString()}</b> Likes: <b>${likeCount.toLocaleString()}</b> Earned Diamonds: <b>${diamondsCount.toLocaleString()}</b>`)
-}
-
-function generateUsernameLink(data) {
-    return `<a class="usernamelink" href="https://www.tiktok.com/@${data.uniqueId}" target="_blank">${data.uniqueId}</a>`;
-}
-
-function isPendingStreak(data) {
-    return data.giftType === 1 && !data.repeatEnd;
-}
-
-/**
- * Add a new message to the chat container
- */
-function addChatItem(color, data, text, summarize) {
-    let container = location.href.includes('obs.html') ? $('.eventcontainer') : $('.chatcontainer');
-
-    if (container.find('div').length > 500) {
-        container.find('div').slice(0, 200).remove();
-    }
-
-    container.find('.temporary').remove();;
-
-    container.append(`
-        <div class=${summarize ? 'temporary' : 'static'}>
-            <img class="miniprofilepicture" src="${data.profilePictureUrl}">
-            <span>
-                <b>${generateUsernameLink(data)}:</b> 
-                <span style="color:${color}">${sanitize(text)}</span>
-            </span>
-        </div>
-    `);
-
-    container.stop();
-    container.animate({
-        scrollTop: container[0].scrollHeight
-    }, 400);
-}
-
-/**
- * Add a new gift to the gift container
- */
-function addGiftItem(data) {
-    let container = location.href.includes('obs.html') ? $('.eventcontainer') : $('.giftcontainer');
-
-    if (container.find('div').length > 200) {
-        container.find('div').slice(0, 100).remove();
-    }
-
-    let streakId = data.userId.toString() + '_' + data.giftId;
-
-    let html = `
-        <div data-streakid=${isPendingStreak(data) ? streakId : ''}>
-            <img class="miniprofilepicture" src="${data.profilePictureUrl}">
-            <span>
-                <b>${generateUsernameLink(data)}:</b> <span>${data.describe}</span><br>
-                <div>
-                    <table>
-                        <tr>
-                            <td><img class="gifticon" src="${data.giftPictureUrl}"></td>
-                            <td>
-                                <span>Name: <b>${data.giftName}</b> (ID:${data.giftId})<span><br>
-                                <span>Repeat: <b style="${isPendingStreak(data) ? 'color:red' : ''}">x${data.repeatCount.toLocaleString()}</b><span><br>
-                                <span>Cost: <b>${(data.diamondCount * data.repeatCount).toLocaleString()} Diamonds</b><span>
-                            </td>
-                        </tr>
-                    </tabl>
-                </div>
-            </span>
-        </div>
-    `;
-
-    let existingStreakItem = container.find(`[data-streakid='${streakId}']`);
-
-    if (existingStreakItem.length) {
-        existingStreakItem.replaceWith(html);
-    } else {
-        container.append(html);
-    }
-
-    container.stop();
-    container.animate({
-        scrollTop: container[0].scrollHeight
-    }, 800);
-}
-
-
-// // viewer stats
-// connection.on('roomUser', (msg) => {
-//     if (typeof msg.viewerCount === 'number') {
-//         viewerCount = msg.viewerCount;
-//         updateRoomStats();
-//     }
-// })
-
-// // like stats
-// connection.on('like', (msg) => {
-//     if (typeof msg.totalLikeCount === 'number') {
-//         likeCount = msg.totalLikeCount;
-//         updateRoomStats();
-//     }
-
-//     if (window.settings.showLikes === "0") return;
-
-//     if (typeof msg.likeCount === 'number') {
-//         addChatItem('#447dd4', msg, msg.label.replace('{0:user}', '').replace('likes', `${msg.likeCount} likes`))
-//     }
-// })
-
-// // Member join
-// let joinMsgDelay = 0;
-// connection.on('member', (msg) => {
-//     if (window.settings.showJoins === "0") return;
-
-//     let addDelay = 250;
-//     if (joinMsgDelay > 500) addDelay = 100;
-//     if (joinMsgDelay > 1000) addDelay = 0;
-
-//     joinMsgDelay += addDelay;
-
-//     setTimeout(() => {
-//         joinMsgDelay -= addDelay;
-//         addChatItem('#21b2c2', msg, 'joined', true);
-//     }, joinMsgDelay);
-// })
-
-// // New chat comment received
-// connection.on('chat', (msg) => {
-//     if (window.settings.showChats === "0") return;
-
-//     addChatItem('', msg, msg.comment);
-// })
-
-// // New gift received
-// connection.on('gift', (data) => {
-//     if (!isPendingStreak(data) && data.diamondCount > 0) {
-//         diamondsCount += (data.diamondCount * data.repeatCount);
-//         updateRoomStats();
-//     }
-
-//     if (window.settings.showGifts === "0") return;
-
-//     addGiftItem(data);
-// })
-
-// // share, follow
-// connection.on('social', (data) => {
-//     if (window.settings.showFollows === "0") return;
-
-//     let color = data.displayType.includes('follow') ? '#ff005e' : '#2fb816';
-//     addChatItem(color, data, data.label.replace('{0:user}', ''));
-// })
+ 
 
 connection.on('streamEnd', () => {
     $('#stateText').text('Stream ended.');
@@ -255,17 +93,23 @@ class rankItem{
     this.DOM.show()
     const order = n+1
     this.DOM.removeClass('top')
-    if(order <= 3) {
-      this.DOM.addClass('top')
-    }
     
     const top = (n * this.DOM.outerHeight()) + (n * 10) + 'px';
     this.DOM.css('top', top);
     this.DOM.find('.number').text(order)
+    
+    if(order <= 3) {
+      this.DOM.addClass('top')
+    }
+    if(order <= 6){
+      this.appendTo($('#likerank .rankitems'))
+    } else {
+      this.DOM.remove()
+    }
   }
 }
 
-const likeRankItems = {}  
+let likeRankItems = {}  
 
 function refreshLikeRanking(){
   const list = []
@@ -290,7 +134,7 @@ connection.on('like', (msg) => {
   if(!user){
     likeRankItems[userId] = new rankItem(msg)
     user = likeRankItems[userId]
-    user.appendTo($('#likerank .rankitems'))
+    // user.appendTo($('#likerank .rankitems'))
   } 
   
   if (typeof msg.totalLikeCount === 'number') {
@@ -304,8 +148,10 @@ connection.on('like', (msg) => {
 }) 
 
 setInterval(function(){
-   for(let userId in likeRankItems){
-     likeRankItems[userId].score = 0;
-   }
+   // for(let userId in likeRankItems){
+   //   likeRankItems[userId].score = 0;
+   // }
+  likeRankItems = {}
+  $('#likerank .rankitems').html(null)
   console.log('reset')
 }, 2*60*1000)
