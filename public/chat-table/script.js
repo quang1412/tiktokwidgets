dcebf0ad31482cba0c02410f61a0ae677b8e3f8c
Glitch (hello-express) <none>
2023-09-17 09:25:27 +0000

    👕🚜 Checkpoint ./public/chat-table/script.js:663034/1856

--- public/chat-table/script.js
+++ public/chat-table/script.js
@@ -140,20 +140,22 @@ $(document).ready(function() {
           },
           autoPrint: false,
           customize: function ( win ) {
-              $(win.document.body)
-                  .css( 'font-size', '10pt' )
-                  .prepend(
-                      '<img src="http://datatables.net/media/images/logo-fade.png" style="position:absolute; top:0; left:0;" />'
-                  );
+            $(win.document.body)
+                .css( 'font-size', '10pt' )
+                .prepend(
+                    '<img src="http://datatables.net/media/images/logo-fade.png" style="position:absolute; top:0; left:0;" />'
+                );
 
-              $(win.document.body).find( 'table' )
-                  .addClass( 'compact' )
-                  .css( 'font-size', 'inherit' );
-            
-              $.each($(win.document.body).find( 'td' ), (e) => {
-                // e.innerHTML.replace('/n','</br>')
-                console.log(e)
-              })
+            $(win.document.body).find( 'table' )
+                .addClass( 'compact' )
+                .css( 'font-size', 'inherit' );
+
+            $.each($(win.document.body).find( 'tr' ), (i, e) => {
+
+            });
+            $.each($(win.document.body).find( 'td' ), (i, e) => {
+                e.innerHTML = e.innerText.replaceAll('\\n','<br>')
+            });
           }
       },
       {
