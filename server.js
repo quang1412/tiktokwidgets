f903fa81185bc9ac9b90eb3c7c365741d1decf98
Glitch (hello-express) <none>
2024-01-02 02:52:07 +0000

    🐥🐺 Checkpoint ./package.json:663034/1478 ./server.js:663034/3680

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-var express = require('express'),
+const express = require('express'),
   app = express(),
   http = require('http'),
   client = require("socket.io-client"),
   socketIO = require('socket.io'),
-  server, io,
+
   ProxyAgent = require('proxy-agent').ProxyAgent;
 
 
@@ -13,10 +13,10 @@ app.get('/', function(req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-server = http.Server(app);
+const server = http.Server(app);
 server.listen(5000);
 
-io = socketIO(server, {
+const io = socketIO(server, {
   cors: {
     origin: "https://quang.codocla.vn",
     methods: ["GET", "POST"]
@@ -36,150 +36,6 @@ const proxies = [
 ]
 
 const roomList = {};
- 
-class TikTokIOConnection {
-  constructor(uniqueId, options) {
-    this.socket = client.connect("https://tiktok-chat-reader.zerody.one");
-    this.uniqueId = uniqueId;
-    this.options = options;
-    this.viewerCount = 0;
-    this.likeCount = 0;
-    this.diamondsCount = 0;
-    this.state = null;
-
-    this.socket.on('connect', () => {
-      console.info("zerodySocket connected!");
-
-      // Reconnect to streamer if uniqueId already set
-      if (this.uniqueId) {
-        // this.setUniqueId();
-        this.reConnect()
-      }
-    })
-
-    this.socket.on('greeting-from-server', (mess) => {
-      console.log(mess);
-    })
-
-    this.socket.on('disconnect', () => {
-      console.warn("Socket disconnected!");
-    })
-
-    this.socket.on('streamEnd', () => {
-      console.warn("LIVE has ended!");
-      this.uniqueId = null;
-      
-      roomList[this.uniqueId] = null
-      delete roomList[this.uniqueId]
-    })
-
-    this.socket.on('tiktokDisconnected', (errMsg) => {
-      console.warn(errMsg);
-      if (errMsg && errMsg.includes('LIVE has ended')) {
-        this.uniqueId = null;
-        
-        roomList[this.uniqueId] = null
-        delete roomList[this.uniqueId]
-      }
-    });
-    
-    this.socket.onAny((eventName, ...args) => {
-      io.of("/app").to(this.uniqueId).emit(eventName, ...args)
-    }) 
-  }
-
-  connect(uniqueId, options) {
-    this.uniqueId = uniqueId;
-    this.options = options || {};
-
-    this.setUniqueId();
-
-    return new Promise((resolve, reject) => {
-      this.socket.once('tiktokConnected', resolve);
-      this.socket.once('tiktokDisconnected', reject);
-
-      setTimeout(() => {
-        reject('Connection Timeout');
-      }, 15000)
-    })
-  }
-
-  setUniqueId() {
-    this.socket.emit('setUniqueId', this.uniqueId, this.options);
-  }
-
-  on(eventName, eventHandler) {
-    this.socket.on(eventName, eventHandler);
-  }
-
-  reConnect() { 
-    if (this.uniqueId !== '') {
-
-      console.log('Connecting...');
-      
-      const randomInt = Math.floor(Math.random() * proxies.length);
-      const randomProxy = proxies[randomInt];
-      
-      this.connect(this.uniqueId, {
-        enableExtendedGiftInfo: true,
-        requestOptions: {
-          httpsAgent: new ProxyAgent(randomProxy),
-          timeout: 10000 // 10 seconds
-        }
-      }).then(state => {
-        console.log(`Connected to roomId ${state.roomId}`);
-        io.of("/app").to(this.uniqueId).emit("tiktokConnected")
-        // reset stats
-        this.viewerCount = 0;
-        this.likeCount = 0;
-        this.diamondsCount = 0;
-        this.state = state;
-        // updateRoomStats();
-
-      }).catch(errorMessage => {
-        console.log(errorMessage);
-
-        // schedule next try if obs username set
-        if (this.uniqueId) {
-          setTimeout(() => {
-            this.reConnect();
-          }, 30000);
-        }
-      })
-
-    } else {
-      console.log('no username entered');
-    }
-
-  }
-}
- 
-io.of('/app').on('connection', function(socket) {
-  let _username;
-  let currentUniqueId;
-  //   const zerodySocket = new TikTokIOConnection();
-
-  socket.on("setUniqueId", function(uniqueId, options) {
-    if(uniqueId && currentUniqueId != uniqueId){
-      socket.join(uniqueId);
-      socket.leave(currentUniqueId);
-      currentUniqueId = uniqueId
-      
-      const existRoom = roomList[uniqueId];
-      if(existRoom){
-        // existRoom.getState
-        socket.emit("tiktokConnected", existRoom.state);
-      } else {
-        roomList[uniqueId] = new TikTokIOConnection(uniqueId, options);
-      } 
-    } 
-  }) 
-  
-  socket.on("disconnect", (reason) => {
-    // ...
-  }); 
- 
-})
 
 io.on('connection', function(socket) {
   console.log('new socket client config');
@@ -202,3 +58,5 @@ io.of('/widget').on('connection', function(socket){
     io.to(widgetId).emit(e, data);
   })
 })
+
+io.of('/control')
